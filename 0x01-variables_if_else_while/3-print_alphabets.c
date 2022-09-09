00ab312e467120a1fe30d0a6cb40a2c2b28718f9
rky-ke <ronaldyegon15@gmail.com>
2022-09-09 14:55:35 -0700

    C Variables and Statements

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	char cha;
 
-	for (cha = 'a' ; cha <= 'z' ; cha++)                                                                                            
+	for (cha = 'a' ; cha <= 'z' ; cha++)
 		putchar(cha);
-        for (cha = 'A' ; cha <= 'Z' ; cha++)
-		       	putchar(cha);	
+       	for (cha = 'A' ; cha <= 'Z' ; cha++)
+		putchar(cha);
 	return (0);
 }
