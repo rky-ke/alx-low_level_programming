111fec5bcf1848b26c4634f3c8ea239e0b6291fe
rky-ke <ronaldyegon15@gmail.com>
2022-09-09 14:51:19 -0700

    C Variables and Statements

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,16 @@
-#include <stdio.h>                                                                                                                      
-
-/**                                                                                                                                     
- * main - Entry point                                                                                                                    *
- *      
- * Return: Always 0 (Success)
+#include <stdio.h>
+/**                                                                                                                      
+ * main - Entry point
  *
+ * Return: Always 0 (Success)
  */
 int main(void)                                                                                                                          
-
-{                                                                                                                                       
+{
 	char cha;
 
 	for (cha = 'a' ; cha <= 'z' ; cha++)                                                                                            
 		putchar(cha);
         for (cha = 'A' ; cha <= 'Z' ; cha++)
 		       	putchar(cha);	
-	return (0);                                                                                             
+	return (0);
 }
