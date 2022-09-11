8e1ca0cf3c569c4937af505c997a83fb803da74f
rky-ke <ronaldyegon15@gmail.com>
2022-09-11 09:49:38 -0700

    C Variables and Statements

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,15 +2,14 @@
 /**
  * main - Entry point
  *
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int d;
+	int d;
 
-for (d = '0' ; d <= '16' ; d++)
-	putchar (d);
-putchar ('\n');
-return (0);
+	for (d = '0' ; d <= '16' ; d++)
+		putchar (d);
+	putchar ('\n');
+	return (0);
 }
