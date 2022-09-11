5bf6e93996daa5d0b045635aba014b5b769740ec
rky-ke <ronaldyegon15@gmail.com>
2022-09-11 13:56:36 -0700

    C Variables and Statements

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,13 @@ int main(void)
 	int a;
 
 	for (a = '0' ; a <= '9' ; a++)
-		putchar (a);
-	if (a != '9')
 	{
-		putchar (',');
-		putchar (' ');
+		putchar (a);
+		if (a != '9')
+		{
+			putchar (',');
+			putchar (' ');
+		}
 	}
 	putchar ('\n');
 	return (0);
