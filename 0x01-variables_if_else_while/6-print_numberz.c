78d7622c617ae779263df04a4b484293d52083d0
rky-ke <ronaldyegon15@gmail.com>
2022-09-11 08:01:40 -0700

    C Variables and Statements

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return : Always 0 (Succes)
+ * Return : Always 0 (Success)
  */
 int main(void)
 {
 	int d;
 
-	for (d = 0 ; d <=9 ; d++)
+	for (d = 0 ; d <= 9 ; d++)
 		putchar (d);
 	putchar ('\n');
 	return (0);
