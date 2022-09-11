0b0b7af65a46d5d035d9273faaa9c8aa012bcc45
rky-ke <ronaldyegon15@gmail.com>
2022-09-11 02:29:57 -0700

    C Variables and Statements

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,19 +4,19 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	char alfa,e,q;
+	char alfa, e, q;
+
 	e = 'e';
 	q = 'q';
 
 	for (alfa = 'a'; alfa <= 'z'; alfa++)
 	{
-		if (alfa != e && alfa !=q)
+		if (alfa != e && alfa != q)
 		{
 			putchar (alfa);
 		}
-	
 	}
 	putchar ('\n');
 	return (0);
