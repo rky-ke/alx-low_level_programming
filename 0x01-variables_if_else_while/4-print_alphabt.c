65858def56be6531292c1664368f0960c8dd8c84
rky-ke <ronaldyegon15@gmail.com>
2022-09-11 02:19:26 -0700

    C Variables and Statements

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char alfa,e,q;
+	e = 'e';
+	q = 'q';
+
+	for (alfa = 'a'; alfa <= 'z'; alfa++)
+	{
+		if (low != e && low !=q)
+		{
+			putchar (low);
+		}
+		putchar('\n');
+	
+	}
+	return (0);
+}
