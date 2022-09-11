64e133e1bbbab5b150c3fcdb7f0eaa3f4e9cab55
rky-ke <ronaldyegon15@gmail.com>
2022-09-11 08:29:38 -0700

    C Variables and Statements

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char alf;
+
+	for (alfa = 'z' ; alfa >= 'a' ; alfa-- )
+		putchar (alfa);
+	putchar ('\n');
+	return (0);
+}
