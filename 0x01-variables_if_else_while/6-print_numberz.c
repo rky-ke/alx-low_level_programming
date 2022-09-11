0a15420cdc2a2888c2b52bfe7309f5008e098754
rky-ke <ronaldyegon15@gmail.com>
2022-09-11 07:29:30 -0700

    C Variables and Statements

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ * Return : Always 0 (Succes)
+ */
+int main(void)
+{
+	int i;
+	for ( i = 0 ; i <=9 ; i++ )
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
