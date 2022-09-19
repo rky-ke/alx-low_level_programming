c3aa776f6bbfdcc64df4b0d39dac34c9425cbe04
rky-ke <ronaldyegon15@gmail.com>
2022-09-19 13:34:43 -0700

    C Pointers and Arrays

---
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 98;
+	b = 42;
+
+	printf("a=%d, b=%d\n", a, b);
+	swap_int(&a, &b);
+	printf("a=%d, b=%d\n", a, b);
+	return (0);
+}
