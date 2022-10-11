2d66bdd2c5531d2d036d57e683e57e8de1c8f47c
rky-ke <ronaldyegon15@gmail.com>
2022-10-11 09:43:36 -0700

    structures, typedef

--- dog.h
+++ dog.h
@@ -1,16 +1,12 @@
-fndef _DOG_H_
+#ifndef _DOG_H_
 #define _DOG_H_
 
 /**
- *
  * struct dog - structure for a basic dog
  * @name: Dog's name
- *
  * @age: Dog's age
  * @owner: Dog's owner
- *
  * Description: data structure for dog
- *
  */
 struct dog
 {
