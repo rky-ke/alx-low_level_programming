a2045ba29fd68b6e5f748890061ac2bec434d47e
rky-ke <ronaldyegon15@gmail.com>
2023-07-24 17:21:30 +0300

    Linear Seach Algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,13 +15,13 @@ int linear_search(int *array, size_t size, int value)
 	int i;
 
 	if (array == NULL) {
-		return -1;
+		return (-1);
 	}
 	for (i = 0; i < (int)size; i++) {
 		printf("Value checked array[%u] = %d\n", i, array[i]);
 		if (array[i] == value) {
-			return i;
+			return (i);
 		}
 	}
-	return -1;
+	return (-1);
 }
