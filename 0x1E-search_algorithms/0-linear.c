8a935cf05b25826c8fd76612d23c543e6e1772ad
rky-ke <ronaldyegon15@gmail.com>
2023-07-24 17:36:38 +0300

    Linear Search Task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+
 /**
  * linear_search - searches for a value in an array of
  * integers using the Linear search algorithm
@@ -8,17 +9,17 @@
  * @value: value to search in
  * Return: Always EXIT_SUCCESS
  */
-int linear_search(int *array, size_t size, int value) 
+int linear_search(int *array, size_t size, int value)
 {
 	int i;
 
-	if (array == NULL) {
+	if (array == NULL)
 		return (-1);
-	}
-	for (i = 0; i < (int)size; i++) 
+
+	for (i = 0; i < (int)size; i++)
 	{
-		printf("Value checked array[%u] = %d\n", i, array[i]);
-		if (array[i] == value)
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (value == array[i])
 			return (i);
 	}
 	return (-1);
