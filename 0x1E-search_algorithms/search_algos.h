a181b073c69288550dc15627b233517d79a35ca8
rky-ke <ronaldyegon15@gmail.com>
2023-07-25 19:36:19 +0300

    Search Algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -32,7 +32,6 @@ typedef struct listint_s
  * Description: singly linked list node structure with an express lane
  * for Holberton project
  */
-
 typedef struct skiplist_s
 {
 	int n;
@@ -40,5 +39,14 @@ typedef struct skiplist_s
 	struct skiplist_s *next;
 	struct skiplist_s *express;
 } skiplist_t;
+
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
 #endif
